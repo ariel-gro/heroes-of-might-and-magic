fad0e54d960b137b25c3b0304a9041a87cf10e06
amir.apel <amir.apel@1a0214c2-0cb2-11de-b321-f573fdaff03e>
2009-03-12 19:23:57 +0000

    Initial import.

---
+++ Heroes/test/tau/heroes/test/MainModuleTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package tau.heroes.test;
+
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * @author Amir
+ *
+ */
+public class MainModuleTest extends TestCase {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
