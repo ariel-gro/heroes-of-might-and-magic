3c6322a6716c09194d134407fbc28552bcd3150c
arielthepriest <arielthepriest@1a0214c2-0cb2-11de-b321-f573fdaff03e>
2009-03-13 09:25:36 +0000

    changed the resource data structure to HashMap took care of ownership changes of a resource took care of updating quantity of a resource per player when a hero takes over a resource. added console printing of ownership changes
    removed testOccupation, since it's related to hero movement

--- trunk/Heroes/src/tau/heroes/Resource.java
+++ trunk/Heroes/src/tau/heroes/Resource.java
@@ -2,26 +2,52 @@ package tau.heroes;
 
 public class Resource {
 
-	public Player owner;
-	int xPos;
-	int yPos;
-	int perDay;
-	public ResourceType type;
+	private Player owner;
+	private final int xPos;
+	private final int yPos;
+	private final ResourceType type;
 
-	public Resource (ResourceType type){
+	public Resource (ResourceType type, int xPos, int yPos){
 		this.type = type;
 		this.owner = null;
-		this.xPos = 7;
-		this.yPos = 9;
+		this.xPos = xPos;
+		this.yPos = yPos;
 	}
 
-	public void setOwner(Player player) {
+	public void setOwner(Player player)
+	{
+		if (this.owner != null)
+		{
+			this.owner.decrementQuantity(this.getType().getTypeName());
+			System.out.println("player "+this.owner.getName()+
+					" lost ownership over "+this.getType().getTypeName()+
+					" at place: ("+this.getXPos()+", "+this.getYPos()+")");
+		}
 		this.owner = player;
-
+		player.incrementQuantity(this.getType().getTypeName());
+		System.out.println("player "+this.owner.getName()+
+				" took ownership over "+this.getType().getTypeName()+
+				" at place: ("+this.getXPos()+", "+this.getYPos()+")");
 	}
 
-	public boolean checkOwner(Hero hero) {
-		return (this.owner.equals(hero.player));
+	public boolean checkOwner(Player player) {
+		return (this.getOwner().equals(player));
+	}
+
+	public Player getOwner() {
+		return owner;
+	}
+
+	public int getXPos() {
+		return xPos;
+	}
+
+	public int getYPos() {
+		return yPos;
+	}
+
+	public ResourceType getType() {
+		return type;
 	}
 
 
--- trunk/Heroes/test/tau/heroes/test/ResourcesTest.java
+++ trunk/Heroes/test/tau/heroes/test/ResourcesTest.java
@@ -6,37 +6,22 @@ public class ResourcesTest extends TestCase
 {
 	Player player1 = new Player("Ariel");
 	Player player2 = new Player("Ido");
-	Resource r = new Resource(ResourceType.WOOD);
+	Resource r = new Resource(ResourceType.WOOD, 7, 9);
 	Hero h1 = new Hero(player2);
 	Hero h2 = new Hero(player2);
 	Hero h3 = new Hero(player1);
-	World w = new World(20, 20);
-
-	public ResourcesTest()
-	{
-		w.setResourcesGrid(r, 7, 9);
-	}
 
 	public void testOwnership()
 	{
-		assertEquals(null, r.owner);
+		assertEquals(null, r.getOwner());
 		r.setOwner(h1.player);
-		assertEquals(h1.player, r.owner);
-		assertEquals(true, r.checkOwner(h2));
-		assertEquals(false, r.checkOwner(h3));
+		assertEquals(1, player2.getQuantity("wood"));
+		assertEquals(h1.player, r.getOwner());
+		assertEquals(true, r.checkOwner(h2.player));
+		assertEquals(false, r.checkOwner(h3.player));
 		r.setOwner(h3.player);
-		assertEquals(true, r.checkOwner(h3));
-		//assertEquals(true, );
+		assertEquals(true, r.checkOwner(h3.player));
 		assertEquals(1, player1.getQuantity("wood"));
+		assertEquals(0, player2.getQuantity("wood"));
 	}
-
-	public void testOccupation()
-	{
-		h1.moveTo(7, 9, w);
-		assertEquals(true, r.checkOwner(h1));
-		h1.moveTo(5, 4, w);
-		h3.moveTo(7, 9, w);
-		assertEquals(true, w.getResourcesGrid(7, 9).checkOwner(h3));
-	}
-
 }
