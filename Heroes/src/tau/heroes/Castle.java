0134eab26bd176a14b2e137c340935beafe38f31
yuval.eitan <yuval.eitan@1a0214c2-0cb2-11de-b321-f573fdaff03e>
2009-03-16 14:00:56 +0000

    Changed CTOR to save reference to board. Implemented getXPos() and getYPos(). Implemented enterHero(Hero hero) to support a hero entering his own castle.

--- Heroes/src/tau/heroes/Castle.java
+++ Heroes/src/tau/heroes/Castle.java
@@ -2,15 +2,34 @@ package tau.heroes;
 
 public class Castle
 {
+	private Board board;
 	private Player player;
 	private int xPos;
 	private int yPos;
 	
-	public Castle(Player player, Board theBoard, int x, int y) {
+	public Castle(Player player, Board board, int x, int y) {
 		this.player = player;
+		this.board = board;
 		this.xPos = x;
 		this.yPos = y;
 		
-		theBoard.placeCastle(this, x, y);
+		this.board.placeCastle(this, x, y);
+	}
+	
+	public void enterHero(Hero hero) {
+		if (hero.player == this.player)
+			enterHeroIntoOwnCastle(hero);
+	}
+	
+	private void enterHeroIntoOwnCastle(Hero hero) {
+		System.out.println(hero.player.getName() + "'s hero has entered his own castle.\n");
+	}
+
+	public int getXPos() {
+		return this.xPos;
+	}
+
+	public int getYPos() {
+		return this.yPos;
 	}
 }
