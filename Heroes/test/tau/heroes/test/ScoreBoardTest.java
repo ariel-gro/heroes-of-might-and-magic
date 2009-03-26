c00132472d7763904f481e33c66a94d39ac17747
sekely <sekely@1a0214c2-0cb2-11de-b321-f573fdaff03e>
2009-03-26 13:27:11 +0000

    high score table manipulation

---
+++ Heroes/test/tau/heroes/test/ScoreBoardTest.java
@@ -0,0 +1,39 @@
+package tau.heroes.test;
+
+import tau.heroes.GameScoreBoard;
+import tau.heroes.Player;
+import junit.framework.TestCase;
+
+public class ScoreBoardTest extends TestCase{
+	
+	Player player1 = new Player("Ido");
+	Player player2 = new Player("Ariel");
+	GameScoreBoard board = new GameScoreBoard();
+	
+	public void testScoreBoard()
+	{
+		board.addToScoreBoard(player1, 15);
+		board.addToScoreBoard(player2, 17);
+		
+		assertEquals(17, board.getScoreAt(0));
+		assertEquals(15, board.getScoreAt(1));
+		assertEquals(player2, board.getPlayerAt(0));
+		assertEquals(player1, board.getPlayerAt(1));
+	}
+	
+	public void testSaveScoreBoard()
+	{
+		board.addToScoreBoard(player1, 15);
+		board.addToScoreBoard(player2, 17);
+		board.save();
+		
+		GameScoreBoard otherBoard = new GameScoreBoard();
+		otherBoard.load();
+		
+		assertEquals(17, board.getScoreAt(0));
+		assertEquals(15, board.getScoreAt(1));
+		assertEquals(player2, board.getPlayerAt(0));
+		assertEquals(player1, board.getPlayerAt(1));
+	}
+
+}
