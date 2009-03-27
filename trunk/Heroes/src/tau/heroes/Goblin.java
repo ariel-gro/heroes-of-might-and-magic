fa489ffa083cb0dda963b59b8f053bf0b7338dd6
rangeler <rangeler@1a0214c2-0cb2-11de-b321-f573fdaff03e>
2009-03-27 17:20:29 +0000

    Exposing the name of the creature as a defined name

--- trunk/Heroes/src/tau/heroes/Goblin.java
+++ trunk/Heroes/src/tau/heroes/Goblin.java
@@ -5,12 +5,13 @@ import java.io.Serializable;
 public class Goblin extends Creature implements Serializable
 {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
+	public static final String GOBLIN_NAME = "Goblin";
 
 	public Goblin(int numberOfUnits)
 	{
-		super(AlignmentType.BARBARIAN, 2, 0, 6, 8, 12, 4, "Goblin", numberOfUnits);
+		super(AlignmentType.BARBARIAN, 2, 0, 6, 8, 12, 4, GOBLIN_NAME, numberOfUnits);
 	}
 }
