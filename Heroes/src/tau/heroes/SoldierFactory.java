8e43693357710d6b288321f7e44a3784441b631a
arielthepriest <arielthepriest@1a0214c2-0cb2-11de-b321-f573fdaff03e>
2009-03-31 20:33:06 +0000

    implemented weekend rules: 1. CreatureFactorys now have unitsPerWeek instead of unitsPerDay. 2. each factory keeps track of the day of the week (one can ask a factory for getDay() and get a String of the day it is in). 3. when a player ends a turn, we iterate over his castles, each castle calls endDay(), which then iterates over the castle's factories and calls their endDay() method. 4. the endDay method takes care of updating the day, and when necessary increases the unitsAvailableToBuild field.
    also
    - refactored some method's names
    - added some tests under CastleTest to make sure the changes are OK

--- Heroes/src/tau/heroes/SoldierFactory.java
+++ Heroes/src/tau/heroes/SoldierFactory.java
@@ -10,10 +10,10 @@ package tau.heroes;
 public class SoldierFactory extends CreatureFactory {
 	private static final long serialVersionUID = 1L;
 	private final static String name = "Soldier Factory";
-	public final static int MAX_UNITS_PER_DAY = 10;
+	public final static int UNITS_PER_WEEK = 10;
 	
 	public SoldierFactory() {
-		super(MAX_UNITS_PER_DAY);
+		super(UNITS_PER_WEEK);
 		this.setPrice(ResourceType.GOLD.getTypeName(), 50);
 		this.setPricePerUnit(ResourceType.GOLD.getTypeName(), 10);
 	}
