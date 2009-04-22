bc4ef6a178ab4dfc5b5b6849d8f88a9cf39c9b27
arielthepriest <arielthepriest@1a0214c2-0cb2-11de-b321-f573fdaff03e>
2009-04-22 17:46:10 +0000

    accidentally changed board size to 14. changed back to 40.

--- Heroes/src/tau/heroes/Constants.java
+++ Heroes/src/tau/heroes/Constants.java
@@ -3,7 +3,7 @@ package tau.heroes;
 public class Constants {
 	public static final int MIN_PLAYERS = 2;
 	public static final int MAX_PLAYERS = 4;
-	public static final int BOARD_SIZE = 14;
+	public static final int BOARD_SIZE = 40;
 	public static final int GOLD_PER_CASTLE = 1000;
 	public static final int GOBLIN_FACTORY_PRICE_GOLD = 100;
 	public static final int GOBLIN_UNIT_PRICE_GOLD = 10;
