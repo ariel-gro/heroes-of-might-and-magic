c714bc25e71eae4884ffb0a9a9df35d15a6912b2
eyalavny <eyalavny@1a0214c2-0cb2-11de-b321-f573fdaff03e>
2009-04-25 18:59:35 +0000

    Initial import.

---
+++ plugin/trunk/heroes.plugin/src/heroes/plugin/actions/ResetHighscoresAction.java
@@ -0,0 +1,36 @@
+package heroes.plugin.actions;
+import heroes.plugin.view.HeroesView;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+
+public class ResetHighscoresAction implements IWorkbenchWindowActionDelegate {
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IWorkbenchWindow window) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run(IAction action) {
+		HeroesView.GUI.resetHighscores();
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ plugin/trunk/heroes.plugin/src/heroes/plugin/view/HeroesView.java
@@ -0,0 +1,49 @@
+package heroes.plugin.view;
+
+//import java.awt.Label;
+
+
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+
+import org.eclipse.ui.part.ViewPart;
+
+import tau.heroes.GameController;
+import tau.heroes.MainModule;
+import tau.heroes.HeroesGui;
+
+public class HeroesView extends ViewPart 
+{
+	
+	public static HeroesGui GUI;
+	
+
+	public HeroesView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		//TODO: here we should creat the game window.
+		GameController game = new GameController(true);
+		GUI = MainModule.runGraphicalView(game, parent);
+		if(GUI == null)
+		{
+			//exit!
+		}
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
